7409299797f983ac3d49c92bec45512a2c6a614c
Aaron Jensen <aaronjensen@gmail.com>
2016-07-26 12:53:54 -0700

    Fix parser tests

--- test/remap/parser_test.exs
+++ test/remap/parser_test.exs
@@ -3,21 +3,25 @@ defmodule Remap.ParserTest do
   alias Remap.Parser
 
   test "parse key only" do
-    assert Parser.parse("foo") == [{:key, "foo"}]
+    assert Parser.parse("foo") == [{:child, {:identifier, :foo}}]
   end
 
   test "parse multiple keys" do
     assert Parser.parse("foo.bar.baz") ==
-      [{:key, "foo"}, {:key, "bar"}, {:key, "baz"}]
+      [
+        {:child, {:identifier, :foo}},
+        {:child, {:identifier, :bar}},
+        {:child, {:identifier, :baz}},
+      ]
   end
 
   test "start with root" do
     assert Parser.parse("$.foo") ==
-      [:root, {:key, "foo"}]
+      [:root, {:child, {:identifier, :foo}}]
   end
 
   test "all children" do
     assert Parser.parse("foo[*]") ==
-      [{:key, "foo"}, {:children, :all}]
+      [{:child, {:identifier, :foo}}, {:children, :all}]
   end
 end
