fb473514cc1014f7d7387f969b8c9cbb7da1ca18
Aaron Jensen <aaronjensen@gmail.com>
2016-07-26 12:13:23 -0700

    Add package information

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Remap.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
      deps: deps()]
   end
 
@@ -18,6 +20,22 @@ defmodule Remap.Mixfile do
     [
       {:credo, "~> 0.4", only: [:dev, :test]},
       {:dialyxir, "~> 0.3", only: [:dev, :test]},
+    ]
+  end
+
+  defp description do
+    """
+    Map maps from maps/lists to another using JSONPath.
+    """
+  end
+
+  defp package do
+    [
+      name: :remap,
+      files: ["lib", "src", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Aaron Jensen"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/substantial/remap"}
     ]
   end
 end
